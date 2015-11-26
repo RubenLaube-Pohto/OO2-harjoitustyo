616a168e2b3f17fb62dcf04f892316fe593a0c15
Ruben Laube-Pohto <h8871@student.jamk.fi>
2015-11-26 16:11:32 +0200

    Refactor solution

--- harjtyo/constants.h
+++ harjtyo/constants.h
@@ -4,10 +4,13 @@
 #include <string>;
 
 const std::string TITLE = "Harkkarainen";
-const std::string FONT_FILE = "arial.ttf";
-const std::string BG_TEXTURE_FILE = "background.png";
-const std::string PLAYER_TEXTURE_FILE = "mech.png";
-const std::string ENEMY_TEXTURE_FILE = "enemy.png";
+const std::string FONT_FILE = "Fonts\\arial.ttf";
+const std::string BG_TEXTURE_FILE = "Textures\\background.png";
+const std::string PLAYER_TEXTURE_FILE = "Textures\\mech.png";
+const std::string ENEMY_TEXTURE_FILE = "Textures\\enemy.png";
+const std::string GAMEOVER_AUDIO = "Audio\\gameover.wav";
+const std::string PLAYER_DEATH_AUDIO = "Audio\\pain.wav";
+const std::string BG_MUSIC = "Audio\\bass-or-die-loop.wav";
 
 const float MOVE_SPEED = 5.0f;
 const int WIDTH = 800;
